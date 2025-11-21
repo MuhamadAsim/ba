812190119c7bc430f514ffdf2b96acd24603a4e3
Muhamad Asim <rao1235251325@gmail.com>
2025-11-22 00:15:59 +0500

    savepoint-chat2

--- src/routes/googleRoutes.js
+++ src/routes/googleRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { getGoogleAuthURL, googleCallback } from "../controllers/customerAuthController.js";
+import { googleCallbackPartner,getGoogleAuthURLShop } from "../controllers/shopAuthController.js";
+
+
 
 const router = express.Router();
 
 router.get("/google", getGoogleAuthURL);
+router.get("/google-partner", getGoogleAuthURLShop);
 router.get("/google-callback", googleCallback);
+router.get("/google-callback-partner", googleCallbackPartner);
+
 
 export default router;
